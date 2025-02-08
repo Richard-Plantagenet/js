ae20d9bb2fc99c4cd1535cf99d975a58269447de
Роман Козловский <KozlovskiyRV@stud.kai.ru>
2025-02-08 13:32:36 +0300

    test

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json()); // Для парсинга JSON
 
-app.post('/', (req, res) => {
-    res.send("OK");
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
 });
 
 // Эндпоинт для приёма данных с датчиков
